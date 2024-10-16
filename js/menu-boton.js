ab2eb90c4cf1be9ae4b52a4ea3b77b9c32bf859c
Jairo Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2024-10-16 18:07:48 -0500

    Vista Recursos y Png , archivo @keryframes
    - Vista recursos (falta menu, y boton en imagen PNG)
    
    - Vista PNG (casi ningun avance, apenas empezando)
    
    - se creo un archivo con el fin de saber como utilizar @keyframes para el movimiento de la imagen del señor en vista "Home"

---
+++ js/menu-boton.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+  }
+  
+  function toggleSubMenu() {
+    const subMenu = document.getElementById("sub-menu");
+    subMenu.classList.toggle("hidden");
+  }
+  
