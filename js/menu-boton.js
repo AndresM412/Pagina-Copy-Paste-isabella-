9e2963512b972cc014679314e12b57f7a856ee79
Jairo Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2024-10-30 10:10:56 -0500

    Resources (actualizado)

--- js/menu-boton.js
+++ js/menu-boton.js
@@ -1,9 +1,11 @@
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("hidden");
-  }
-  
-  function toggleSubMenu() {
-    const subMenu = document.getElementById("sub-menu");
-    subMenu.classList.toggle("hidden");
-  }
+const menuButton = document.getElementById('menu-button');
+const dropdownMenu = document.getElementById('dropdown-menu');
+
+menuButton.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('show-menu');
+});
+
+
+
+
+
